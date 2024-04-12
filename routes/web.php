3e683224f13a6426af1288952e722785375aa285
tuyandre <tuyandre2018@gmail.com>
2024-04-12 09:27:39 +0200

    mk admin

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,23 @@ Route::get('/team/{id}', 'App\Http\Controllers\TeamController@show')->name('team
 Route::post('/save/message', 'App\Http\Controllers\FeedbackController@store')
     ->name('message.save');
 
-Auth::routes();
+//Auth::routes();
+// Equivalent to Auth::routes()
+Route::get('/login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
+Route::get('/register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'App\Http\Controllers\Auth\RegisterController@register');
+
+Route::get('/password/reset', 'App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/email', 'App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset/{token}', 'App\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('/password/reset', 'App\Http\Controllers\Auth\ResetPasswordController@reset');
+
+Route::get('/email/verify', 'App\Http\Controllers\Auth\VerificationController@show')->name('verification.notice');
+Route::get('/email/verify/{id}', 'App\Http\Controllers\Auth\VerificationController@verify')->name('verification.verify');
+Route::get('/email/resend', 'App\Http\Controllers\Auth\VerificationController@resend')->name('verification.resend');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
